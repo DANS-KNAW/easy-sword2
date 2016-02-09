c3b7c048808cc1c0a6e6e97f4a1830b6af11a38c
Joke Pol <joke.pol@dans.knaw.nl>
2016-02-09 14:47:18 +0100

    fixed copyright header

--- src/test/scala/nl/knaw/dans/api/sword2/AuthenticationSpec.scala
+++ src/test/scala/nl/knaw/dans/api/sword2/AuthenticationSpec.scala
@@ -1,19 +1,18 @@
-/*******************************************************************************
-  * Copyright 2015 DANS - Data Archiving and Networked Services
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *   http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  ******************************************************************************/
-
+/**
+ * Copyright (C) 2015-2016 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.knaw.dans.api.sword2
 
 import nl.knaw.dans.api.sword2.Authentication.hash
@@ -21,18 +20,18 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class AuthenticationSpec extends FlatSpec with Matchers {
 
-  val command = "echo -n SomePassword | openssl sha1 -hmac someUserNameAsSalt -binary | base64"
+  val command = "echo -n 'SomePassword' | openssl sha1 -hmac 'someUserNameAsSalt' -binary | base64"
   val output = "WjYViDQOdGR8V1kkTs900ZfoLXU="
 
-  "hash" should s"return same as '$command'" in {
+  "hash" should s"return same as: $command" in {
     hash("SomePassword", "someUserNameAsSalt") shouldBe output
   }
 
-  it should "return something else than '$command'" in {
+  it should s"return something else than: $command" in {
     hash("somePassword", "someUserNameAsSalt") should not be output
   }
 
-  it should s"not return same as '$command'" in {
+  it should s"not return same as: $command" in {
     hash("SomePassword", "SomeUserNameAsSalt") should not be output
   }
 }
