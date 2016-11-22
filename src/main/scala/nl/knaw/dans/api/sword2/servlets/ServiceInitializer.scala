776d32cde712fa0755293fab02a6ba3caf7d8545
vesaa <vesa.akerman@dans.knaw.nl>
2016-11-22 14:46:01 +0100

    Checking bag-store base-dir and base-url in ServiceInitializer; BagStoreSettings as Option in Settings.

--- src/main/scala/nl/knaw/dans/api/sword2/servlets/ServiceInitializer.scala
+++ src/main/scala/nl/knaw/dans/api/sword2/servlets/ServiceInitializer.scala
@@ -22,7 +22,7 @@ import javax.servlet.{ServletContextEvent, ServletContextListener, ServletExcept
 
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.joran.JoranConfigurator
-import nl.knaw.dans.api.sword2.{DepositHandler, LdapAuthSettings, Settings, SingleUserAuthSettings}
+import nl.knaw.dans.api.sword2.{BagStoreSettings, DepositHandler, LdapAuthSettings, Settings, SingleUserAuthSettings}
 import org.apache.commons.configuration.PropertiesConfiguration
 import org.slf4j.LoggerFactory
 
@@ -62,7 +62,6 @@ class ServiceInitializer extends ServletContextListener {
       case t: Throwable => println(s"Error during logging configuration $t"); throw t
     }
   }
-
   def readSettings(homeDir: File): Settings = {
     val config = {
       val ps = new PropertiesConfiguration()
@@ -86,13 +85,19 @@ class ServiceInitializer extends ServletContextListener {
       case _ => throw new RuntimeException(s"Invalid authentication settings: ${config.getString("auth.mode")}")
     }
     val urlPattern = Pattern.compile(config.getString("url-pattern"))
-    // TODO: make use of bag-store optional through configuration
     val bagStoreBaseUri = config.getString("bag-store.base-url") // TODO: make File, check existence
     val bagStoreBaseDir = config.getString("bag-store.base-dir") // TODO: make File, check existence
+    var bagStoreSettings = None: Option[BagStoreSettings]
+    if (bagStoreBaseUri.trim.nonEmpty || bagStoreBaseDir.trim.nonEmpty) {
+      if (bagStoreBaseDir.trim.isEmpty) throw new RuntimeException("Only bag store base-url given, bag store base-directory missing")
+      if (bagStoreBaseUri.trim.isEmpty) throw new RuntimeException("Only bag store base-directory given, bag store base-url missing")
+      bagStoreSettings = Some(BagStoreSettings(bagStoreBaseDir, bagStoreBaseUri))
+    }
+
     val supportMailAddress = config.getString("support.mailaddress")
     Settings(
       depositRootDir, depositPermissions, tempDir, baseUrl, collectionIri, auth, urlPattern,
-      bagStoreBaseUri, bagStoreBaseDir, supportMailAddress)
+      bagStoreSettings, supportMailAddress)
   }
 
   override def contextDestroyed(sce: ServletContextEvent): Unit = {}
