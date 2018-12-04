54c3489b22a5432c01f2a97ef0cbc5981be264f7
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2018-12-04 14:49:47 +0100

    Rename identifier.dans-doi-registered (#113)

--- src/main/scala/nl.knaw.dans.easy.sword2/DepositProperties.scala
+++ src/main/scala/nl.knaw.dans.easy.sword2/DepositProperties.scala
@@ -52,7 +52,7 @@ class DepositProperties(depositId: DepositId, depositorId: Option[String] = None
     else {
       props.setProperty("bag-store.bag-id", depositId)
       props.setProperty("creation.timestamp", DateTime.now(DateTimeZone.UTC).toString(dateTimeFormatter))
-      props.setProperty("identifier.doi.registered", "no")
+      props.setProperty("identifier.dans-doi.registered", "no")
     }
     debug(s"Using deposit.properties at $file")
     depositorId.foreach(props.setProperty("depositor.userId", _))
